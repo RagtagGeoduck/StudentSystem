9bf0bd842c0de8d55cbb97979618fe59d3e1bf94
RagtagGeoduck <gjt675185496@gmail.com>
2018-10-07 18:36:49 +0800

    add Config.php, to connect the database

---
+++ public/Config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liujunhang
+ * Date: 2018/6/19
+ * Time: 19:58
+ */
+
+// 连接数据库必备参数 
+
+function config(){
+    $configList = [
+        "host"=>"localhost", //mysql地址  
+        "port"=>"3306", // mysql端口号
+        "user"=>"root", // mysql用户名
+        "passwd"=>"", // mysql 密码
+        "dbname"=>"sx01" // 项目所使用的数据库
+    ];
+
+    return $configList;
+}
