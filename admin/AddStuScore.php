fbc47264d70cf9086dc0cda12c55906b949cf150
RagtagGeoduck <gjt675185496@gmail.com>
2018-10-08 14:24:14 +0800

    add AddStuScore PHP files,sql has some problems

--- admin/AddStuScore.php
+++ admin/AddStuScore.php
@@ -123,7 +123,17 @@
                     ?>
                 </div>
                 <br />
-                <div id="visits" style="width:80%;height:300px;"></div>
+                <div id="visits" style="width:80%;height:300px;">
+                    <?php
+                    require_once "../public/Config.php";
+                    $sql = "select distinct KCM from KCB";
+                    $result = mysqli_query($conn, $sql);
+//                    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+                    echo "<pre>";
+                    var_dump($result);
+//                    var_dump($row)
+                    ?>
+                </div>
             </div>
         </div>
     </div>
@@ -134,121 +144,4 @@
 </div>
 </body>
 </html>
-<?php
-$num = @$_POST['StuNumber'];
-$XH = @$_POST['h-StuNum'];
-$name = @$_POST['StuName'];
-$sex = @$_POST['Sex'];
-$birthday = @$_POST['Birthday'];
-$project = @$_POST['Project'];
-$points = @$_POST['StuZXF'];
-$note = @$_POST['StuBZ'];
-$tmp_file = @$_FILES['file']['tmp_name'];
-$handle = @fopen($tmp_file,'r');
-
-//将图片转化为二进制
-$picture = @addslashes(fread($handle, filesize($tmp_file)));
-// 正则表达式验证日期格式
-$checkbirthday = preg_match('/^\d{4}-(0?\d|1?[012])-(0?\d|[12]\d|3[01])$/', $birthday);
-
-// 函数验证表单数据正确性
-function test($num, $name, $checkbirthday, $tmp_file){
-    if($num == NULL){
-        echo "<script>alert('学号不能为空!');location.href='AddStu.php';</script>";
-        exit;
-    }
-    else if($name == NULL){
-        echo "<script>alert('姓名不能为空!');location.href='AddStu.php';</script>";
-        exit;
-    }
-    else if($checkbirthday){
-        echo "<script>alert('日期格式错误!');location.href='AddStu.php';</script>";
-        exit;
-    }
-    else{
-        // 如果上传照片
-        if($tmp_file){
-            $type = @$_FILES['file']['type'];
-            $Psize = @$_FILES['file']['size'];
-            // 判断图片格式
-            if ((($type!="image/gif")&&($type!="image/jpeg")&&($type!="image/pjpeg")&&($type!="image/bmp"))){
-                echo "<script>alert('照片格式不对!');location.href='AddStu.php';</script>";
-                exit;
-            }
-            else if($Psize > 1000000){
-                echo "<script>alert('照片尺寸太大，无法上传!');location.href='AddStu.php';</script>";
-                exit;
-            }
-        }
-    }
-}
-
-// 按钮功能修改
-if(@$_POST['b']=='修改'){
-    echo "<script>if(confirm('确认修改')){}</script>";
-    test($num, $name, $checkbirthday, $tmp_file);
-    if($num != $XH){
-        echo "<script>alert('学号与元数据有异,无法修改!');location.href='AddStu.php';</script>";
-    }else{
-        if(!$tmp_file){
-            $update_sql = "update XSB set XM = '$name',XB = $sex, CSSJ='$birthday',ZY='$project',BZ = '$note' WHERE XH = '$XH'";
-        }else{
-            $update_sql = "update XSB set XM = '$name', XB = $sex, CSSJ='$birthday', ZY='$project', BZ = '$note', ZP = '$picture' WHERE XH = '$XH'";
-        }
-        // 对sql 语句进行更新，原代码被弃用
-        $update_result = mysqli_query($conn,$update_sql);
-        if(mysqli_affected_rows($conn)!=0)
-            echo "<script>alert('修改成功!');location.href='AddStu.php';</script>";
-        else
-            echo "<script>alert('修改成功,请检查输入信息!');location.href='AddStu.php';</script>";
-    }
-}
-// 单击 【添加】按钮
-
-if(@$_POST["b" == '添加']){
-    test($num, $name, $checkbirthday, $tmp_file);
-    // 从学生表中选择对应学号的信息
-    $s_sql = "select XH from XSB WHERE XH='$num'";
-    // 更新 sql 语句
-    $s_result = mysqli_query($conn, $s_sql);
-    // 更新 mysqli_fetch_array 语句
-    $s_row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    if($s_row)
-        echo "<script>alert('学号已存在,无法添加!');location.href='AddStu.php';</script>";
-    else{
-        if(!$tmp_file){
-            $insert_sql = "insert into XSB(XH, XM, XB, CSSJ, ZY, ZXF, BZ) VALUES ('$num', '$name', '$sex', '$birthday', '$project', 0, '$note')";
-
-        }else{
-            $insert_sql = "insert into XSB(XH, XM, XB, CSSJ, ZY, ZXF, BZ, ZP) VALUES ('$num', '$name', '$sex', '$birthday', '$project', 0, '$note', '$picture')";
-        }
-        $insert_sql = mysqli_query($conn, $insert_sql);
-        // 更新sql 语句
-        if(mysqli_affected_rows($conn)!=0)
-            echo "<script>alert('添加成功!');location.href='AddStu.php';</script>";
-        else
-            echo "<script>alert('添加失败,请检查输入信息!');location.href='AddStu.php';</script>";
-    }
-}
-
-// 单击 [删除] 按钮
-if(@$_POST["b" == '删除']){
-    if($num == NULL){
-        echo "<script>alert('请输入要删除的学号!');location.href='AddStu.php';</script>";
-    }else{
-        $d_sql = "select XH from XSB WHERE XH = '$num'";    //查找学生信息
-        // 更新sql 语句
-        $d_result = mysqli_query($conn, $d_sql);
-        $d_row = mysqli_fetch_array($d_resul, MYSQLI_ASSOC);
-        if(!$d_row)
-            echo "<script>alert('学号不存在,无法删除!');Location.href='AddStu.php';</script>";
-        else{
-            $del_sql = "delete from XSB WHERE XH = '$num'";
-            $del_result = mysqli_query($conn, $del_sql) or die('删除失败');
-            if($del_sql)
-                echo "<script>alert('删除学号".$num."成功!');location.href='AddStu.php';</script>";
-        }
-    }
-}
-?>
 
--- admin/InsertScore.php
+++ admin/InsertScore.php
@@ -5,3 +5,30 @@
  * Date: 2018/10/8
  * Time: 1:20 PM
  */
+require_once "../public/Config.php";
+$KCName = $_GET['KCName'];
+$ZYName = $_GET['ZYName'];
+echo "<br><div align='center'>$KCName</div>";
+echo "<table width='450' border='1' align='center' cellpadding='0' cellspacing='0'>";
+echo "<tr bgcolor='#CCCCCC' align='center'><td>学号</td>";
+echo "<td>姓名</td>";
+echo "<td>成绩</td>";
+echo "<td width='160'>操作</td></tr>";
+if(!$KCName&&!$ZYName){
+    for($i = 0; $i < 10; $i ++){
+        echo "<tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>";
+    }
+}else{
+    if($KCName == "请选择")
+        echo "<script>alert('请选择课程');location.href='AddStuScore.php'</script>";
+    else{
+        $total = 0;
+        if($ZYName == "请选择")
+            // 查找学号、姓名所在的行数
+            $XS_sql = "select XH, XM from XSB";
+        else{
+            $XS_sql="select XH, XM from XSB WHERE ZY ='$ZYName'";
+        }
+    }
+}
+
